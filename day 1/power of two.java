ae7b7e6a008e2c20990684f8f2685481e9506e3d
TK-ed <109659829+TK-ed@users.noreply.github.com>
2022-12-23 09:31:12 +0530

    Add files via upload

---
+++ day 1/power of two.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+        
+          if(n==1){
+              return true;
+          }
+        
+    else if(n%2!=0 || n<=0){
+        
+        return false;
+       }
+        
+        
+    while(n>1)
+    {
+        if(n%2!=0)
+            return false;  
+        
+        n=n/2; 
+    }
+   
+    return true;
+}
+    }
